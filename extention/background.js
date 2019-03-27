f5f3a9f9ee23c13eb4084e304d3b8e5aa6d8d587
Mickey <st063775@student.spbu.ru>
2019-03-27 03:47:05 +0300

    Added several features to popup

--- extention/background.js
+++ extention/background.js
@@ -1,3 +1,5 @@
+'use strict';
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
         console.log("The color is green.");
@@ -6,6 +8,14 @@ chrome.runtime.onInstalled.addListener(function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
                 pageUrl: {hostEquals: 'developer.chrome.com'},
+            })
+            ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals: 'vk.com'},
             })
             ],
                 actions: [new chrome.declarativeContent.ShowPageAction()]
--- extention/popup.js
+++ extention/popup.js
@@ -1,13 +1,71 @@
-let getClickedElement = document.getElementById('getClickedElement');
+'use strict'
 
-let funcCode = "document.addEventListener('click', function(e) {    e = e || window.event;    var target = e.target || e.srcElement,        text = target.textContent || target.innerText;   console.log(text);}, false);"
+let chooseProduct = document.getElementById('chooseProduct');
 
-getClickedElement.onclick = function(element) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: funcCode});
+let attributes = [{
+        'name': 'Name', 
+        'text': 'Please choose name of the product'
+    }, {
+        'name': 'Price',
+        'text': 'Please choose price of the product'
+    }, {
+         'name': 'Image',
+         'text': 'Please choose product image'
+    }
+]
+
+let attrElements = []
+
+let iter = 0
+
+let json = []
+
+attributes.forEach(element => {
+
+    let attr = document.createElement('div')
+
+    let text = document.createElement('div')
+    attr.appendChild(text)
+    text.textContent = element.text
+
+    let skip = document.createElement('button')
+    attr.appendChild(skip)
+    skip.textContent = 'Skip'
+
+    skip.addEventListener('click', function() {
+        attr.style.display = 'none'
+        if (++iter < attrElements.length) {
+            attrElements[iter].style.display = 'flex'
+        }
+        else {
+            chooseProduct.style.display = 'flex'
+            alert(json)
+            iter = 0
+        }
     });
-};
 
+    attrElements.push(attr)
 
+    attr.style.display = 'none'
+
+    document.body.appendChild(attr)
+
+    let mouseScript
+
+    mouseScript = chrome.tabs.executeScript(null, {
+        file: 'mouse_handlers.js'
+    }, function() {
+        promise.then(result => {
+            json.push(result.textContent)
+        })
+    })
+
+    mouseScript.then(result => {
+        json.push(result.textContent)
+    })
+})
+
+chooseProduct.addEventListener('click', function() {
+    chooseProduct.style.display = 'none'
+    attrElements[iter].style.display = 'flex'
+})
