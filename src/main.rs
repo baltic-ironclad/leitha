462ce0e83f6c9563b9d168669dcd105476fd5d5c
dikuchan <kdv99@mail.ru>
2019-05-18 00:17:35 +0300

    Scraping update

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
-extern crate reqwest; // Send HTTP requests
-extern crate select; // Inspect element
+#![allow(unused_variables)]
 
-extern crate clap; // Work with command line
+pub mod scraper;
+pub mod storage;
 
-extern crate serde_derive;
-extern crate serde; // Serialize and deserialize
-extern crate serde_json; // Read and write JSON
-
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
-
-pub fn get_page(url: &str) -> String {
-    let client = reqwest::Client::new();
-    let mut request = client.get(url);
-    let mut response = request.send().unwrap();
-
-    assert!(response.status().is_success());
-    return response.text().unwrap();
-    // TODO: Parse item
-}
-
-pub fn get_data(name: &str) {
-    let file_path = Path::new("data").join(name);
-    let file = File::open(file_path).expect("File not found");
-    // TODO: Process JSON data
-}
+// TODO: Parse JSON       (X)
+//       Get page content (X)
+//       Get items        (X)
+//       Update items     (.)
+//
+//       Send updates to Discord (.)
+//       Get info from client    (.)
 
 fn main() {}
---
+++ src/scraper.rs
@@ -0,0 +1,73 @@
+use std::{
+    fs::File,
+    io::{Read, BufReader},
+    path::PathBuf
+};
+use scraper::{Html, Selector};
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize)]
+pub struct Descriptor {
+    url: String,
+    tag: String,
+    class: String
+}
+
+impl Descriptor {
+    pub fn new(filepath: &str) -> Descriptor {
+        let result = read_json(filepath)
+            .expect("Specified JSON is not valid");
+
+        // Try converting JSON to Struct
+        let result: Descriptor = serde_json::from_value(result)
+            .expect("JSON cannot be converted to Descriptor");
+
+        result
+    }
+
+    // FIXME: Kinda ugly
+    pub fn string(self) -> String {
+        let mut result = self.tag;
+        result += &(".".to_owned() + &self.class);
+
+        result
+    }
+
+    pub fn scrape(self) -> Vec<String> {
+        let mut result = Vec::new();
+        let client = reqwest::Client::new();
+        let request = client.get(&self.url);
+
+        // Handle response
+        let mut response = request.send().expect("Response was not supplied");
+        assert!(response.status().is_success());
+        let response = response.text().expect("Response cannot be formatted"); 
+        
+        let document = Html::parse_document(&response);
+
+        let selector = Selector::parse(&self.string()).expect("Selector is not valid");
+
+        // Get list of elements with specified selector
+        for element in document.select(&selector) {
+            result.push(element.text().collect::<Vec<_>>()
+                .first().expect("No element found for specified selector").to_string());
+        }
+        
+        result
+    } 
+}
+
+pub fn read_json(filepath: &str) -> Result<serde_json::Value, serde_json::error::Error> {
+    let filepath: PathBuf = ["data", filepath].iter().collect();
+    let mut file = File::open(filepath).expect("Cannot open file");
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).expect("Cannot read file");
+
+    // Try casting String to JSON    
+    let json: serde_json::Value = serde_json::from_str(&contents[..])
+        .expect("Error occured while parsing"); 
+    
+    Ok(json)
+}
